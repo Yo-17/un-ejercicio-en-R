1db4fbf9cbf69ca5f410b25cddbef1a963b5483b
A human <yolandamtz17@gmail.com>
2024-04-24 03:11:42 +0000

    Elemplos de la Sesion

--- Ejemplo2.R
+++ Ejemplo2.R
@@ -31,3 +31,77 @@ typeof(df) # closure
 mean(df$Population)
 
 df %>% filter(CountryCode == 'MEX', Name == 'Tijuana') %>% head(5)
+
+
+# Ejemplo 3
+
+db_3 <- dbConnect(
+  RMySQL::MySQL(),
+  dbname = "shinydemo",
+  host = "shiny-demo.csa7qlmguqrf.us-east-1.rds.amazonaws.com",
+  username = "guest",
+  password = "guest"
+)
+
+rs <- dbSendQuery(db_3, "SELECT * FROM City Limit 5")
+rs
+
+city_df <- dbFetch(rs)
+
+#desconectar de la bd
+
+dbClearResult(rs)
+dbDisconnect(db_3)
+
+
+# Ejemplo 4
+
+install.packages("rjson")
+library(rjson)
+
+json <- fromJSON(file = 'json_ejemplo4.json')
+
+str(json)
+
+json$Pets[2]
+
+as.data.frame(json)
+
+# XML
+
+install.packages("XML")
+library(XML)
+
+link <- "http://www-db.deis.unibo.it/courses/TW/DOCS/w3schools/xml/cd_catalog.xml"
+
+xml_file <- xmlTreeParse(link)
+
+summary(xml_file)
+
+top_xml <- xmlSApply(xml_file, function(x) xmlSApply(x, xmlValue))
+
+xml_df <- data.frame(t(top_xml), row.names = NULL)
+
+xml_df$PRICE <- as.numeric(xml_df$PRICE)
+
+
+#html
+
+install.packages("rvest")
+library(rvest)
+
+url <- "https://solarviews.com/span/data2.htm"
+html <- read_html(url)
+
+tables <- html_nodes(html, "table")
+
+table_html <- html_table(tables[4], fill = TRUE)
+
+table_html
+
+
+
+
+
+
+
